cc2e253898d047c528aa20199f88e2822f6d7ed0
Eduardo & Tiago <eduardo_ribeiro2003@hotmail.com>
2022-03-31 16:39:27 +0100

    Fix Sub Operation

--- src/main/java/pt/esgts/eng/calculadora/lib/Operations.java
+++ src/main/java/pt/esgts/eng/calculadora/lib/Operations.java
@@ -22,9 +22,9 @@ public class Operations {
     }
 
     public static double sub(List <Double> numbers){
-        int result = 0;
+        Double result = numbers.get(0);
 
-        for(int i = 0; i < numbers.size(); i++){
+        for(int i = 1; i < numbers.size(); i++){
             result -= numbers.get(i);
         }
         return result;
